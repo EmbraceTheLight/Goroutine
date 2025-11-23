019cc97e8a29ebf3f791bab97105b3c8e0afc2dd
ZEY <1010642166@qq.com>
2025-11-23 20:53:15 +0800

    feat(atomic variable): 使用 互斥锁/原子操作 来统计字母频率，测试并发性能。原子操作性能要优于互斥锁。

--- atomic_letter_frequency/letter_frequency.go
+++ atomic_letter_frequency/letter_frequency.go
@@ -1,4 +1,5 @@
-// 统计 200 篇 RFC 文档中每个字母的出现次数
+// demo: 统计 200 篇 RFC 文档中每个字母的出现次数
+// 这个 demo 用于测试单线程 / 使用 WaitGroup + Mutex 多线程/ 原子操作+多线程之间的性能对比
 package main
 
 import (
@@ -6,30 +7,49 @@ import (
 	"io"
 	"net/http"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
 
-func countLetters(url string, frequency *[26]int) {
+var mutex sync.Mutex
+
+func countLetters(url string, frequency *[26]int32, wg *sync.WaitGroup) {
 	resp, _ := http.Get(url)
-	defer resp.Body.Close()
+	defer func() {
+		wg.Done()
+		resp.Body.Close()
+	}()
 	body, _ := io.ReadAll(resp.Body)
-	for _, b := range body {
-		c := strings.ToLower(string(b))
-		index := strings.Index(allLetters, c)
-		if index >= 0 {
-			frequency[index]++
+
+	// 重复搜索 20 遍文档，模拟比较大的文档
+	for i := 0; i < 20; i++ {
+		for _, b := range body {
+			c := strings.ToLower(string(b))
+			index := strings.Index(allLetters, c)
+			if index >= 0 {
+				// 互斥锁
+				//mutex.Lock()
+				//frequency[index]++
+				//mutex.Unlock()
+
+				// 原子操作
+				atomic.AddInt32(&frequency[index], 1)
+			}
 		}
 	}
 }
 func main() {
-	var frequency [26]int
+	var frequency [26]int32
+	var wg sync.WaitGroup
 	start := time.Now()
 	for i := 1000; i <= 1200; i++ {
-		fmt.Println("Fetching RFC", fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i))
-		countLetters(fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i), &frequency)
+		wg.Add(1)
+		go countLetters(fmt.Sprintf("https://www.rfc-editor.org/rfc/rfc%d.txt", i), &frequency, &wg)
 	}
+	wg.Wait()
 	elapsed := time.Since(start)
 	fmt.Println("Done")
 	fmt.Printf("Processing took %s\n", elapsed)
